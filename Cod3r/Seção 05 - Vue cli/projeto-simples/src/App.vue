1059c9462d62cbcb907efadc44c149cc3f97b3c9
Joas <joas7777777@hotmail.com>
2020-07-25 17:03:14 -0300

    Seçao 06

---
+++ Cod3r/Seção 05 - Vue cli/projeto-simples/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{titulo}}
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return {
+      titulo: 'Teste Data usando Vue js '
+    }
+  }
+}
+</script>
+
+<style>
+  #app{
+    background-color: chocolate;
+  }
+</style>
